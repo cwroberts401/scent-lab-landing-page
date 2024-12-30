93920f20893365050b63cf2b6deececdd9f13f2f
Carl Roberts <cwroberts401@gmail.com>
2024-12-30 07:42:29 -0500

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://cwroberts401.github.io',
-    base: '/scent-lab-landing-page/',
+    base: '/scent-lab-landing-page',
 });
