23f3a390d6d93933a609792554c51970cd7c347f
Carl Roberts <cwroberts401@gmail.com>
2025-01-01 10:03:21 -0500

    adding CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cwroberts401.github.io',
-    base: '/scent-lab-landing-page',
+    site: 'https://shopscentlab.com',
 });
